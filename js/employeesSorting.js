10d6978027579ce8fe780f6a63016f468860e8a6
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-04-20 14:59:53 +0200

    Sorting by first name

--- js/employeesSorting.js
+++ js/employeesSorting.js
@@ -3,6 +3,11 @@ const filterWithAge = a => a.id != undefined && a.id != null
 const filterWithoutAge = a => a.id === undefined || a.id === null
 const sortByAgeAscending = (a, b) => a.id > b.id
 
+const sortByNameAscending = (a, b) => a.firstName > b.firstName
+const sortByNameDescending = (a, b) => b.firstName > a.firstName
+const filterWithName = a => a.firstName != undefined && a.firstName != null
+const filterWithoutName = a => a.firstName === undefined || a.firstName === null
+
 const employeeSorter = (sorter, filter, withoutFilter) => {
     let sortedEmployees = Object.values(getEmployeeList(employeeList));
     let employeeWithoutAge = sortedEmployees.filter(withoutFilter)
@@ -19,3 +24,9 @@ const sortAscedning = () => {
     employeeSorter(sortByAgeAscending, filterWithAge, filterWithoutAge)
 }
 
+const sortByFirstNameAlphabetically = () => {
+    employeeSorter(sortByNameAscending, filterWithName, filterWithoutName)
+}
+const sortByFirstNameNonAlphabetically = () => {
+    employeeSorter(sortByNameDescending, filterWithName, filterWithoutName)
+}
