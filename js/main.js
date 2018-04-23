24a4f90f97f5576302b6604d5ff4bc6e6123736b
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-04-23 22:04:17 +0200

    Add new employe to the table

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const renderEmployee = () => {
     renderHtml(
         document.getElementsByClassName('fixed-table_body')[0],
@@ -17,11 +16,49 @@ const renderEmployee = () => {
 }
 const renderHtml = (parentElement, html) => parentElement.innerHTML = html;
 
-const setState = (list) =>{
+const setState = (list) => {
     employeeList = list;
     return employeeList;
 }
-const getEmployeeList = (employeeList) =>{
+const getEmployeeList = (employeeList) => {
     return employeeList;
 }
+const getEmployee = () => {
+    
+    let currentEmployeeList = getEmployeeList(employeeList);
+    const information = [];
+    for (var i = 1; i <= 5; i++) {
 
+        information[i] = document.getElementsByTagName("input")[i].value;
+    }
+
+
+    const data = {
+        "id": (currentEmployeeList.length + 1),
+        "firstName": information[1],
+        "lastName": information[2],
+        "dateOfBirth": information[3],
+        "company": information[4],
+        "note": information[5]
+    }
+
+    return data;
+}
+
+
+
+const onAddNewEployeeButtonClick = (event) => {
+    let currentEmployeeList = getEmployeeList(employeeList);
+    if (getEmployee().firstName.length === 0 && getEmployee().lastName.length === 0 &&
+        getEmployee().dateOfBirth.length === 0 && getEmployee().company.length === 0 && getEmployee().note.length === 0) {} else {
+
+        currentEmployeeList.push(getEmployee());
+        renderEmployee(currentEmployeeList);
+        return setState(currentEmployeeList);
+    }
+}
+const setValueOfID = () => {
+
+    document.getElementsByTagName("input")[0].value = (employeeList.length + 1)
+
+}
